4f6c5179ecdaa5abdf1969b229f27be6429082e1
Javier Gonzalez Jr. <javiergonzalezjr.123@gmail.com>
2026-03-22 16:21:43 -0700

    packaged with electron

---
+++ main.js
@@ -0,0 +1,29 @@
+//tellinig electron how to run my app
+
+const{ app, BrowserWindow } = require('electron');
+const path = require('path');
+
+//this functin create hte actual app window
+function createWindow(){
+    const win = new BrowserWindow({
+        width:1280,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false
+        }
+    });
+
+    // tell electron to load index.hrml as the starting page
+    win.loadFile('index.html');
+}
+
+//make window when electron is ready
+app.whenReady().then(() => {
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){ //'darwin' is Mac bro what
+        app.quit;
+    }
+});
